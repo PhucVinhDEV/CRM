f579a412a1b43b65071a0f7dd6fa8cdcceaa68f5
oIoVinhGa <tpvinh292931@gmail.com>
2024-12-31 21:51:44 +0700

    feat: Oauth2Service with Google

--- CRM-BE/Identity/src/main/java/com/example/CRM/CRMApplication.java
+++ CRM-BE/Identity/src/main/java/com/example/CRM/CRMApplication.java
@@ -5,12 +5,14 @@ import com.example.CRM.user.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
+@EnableFeignClients(basePackages = "com.example.CRM.Oauth2.httpclient")
 public class CRMApplication {
 
 	public static void main(String[] args) {
